37cdeeea6f83ce70af1cb2d3e77ef7ccca5a973e
Anton Maliksi <97695146+antonmaliksi@users.noreply.github.com>
2022-03-18 14:57:37 -0400

    Add files via upload
    Re-uploaded for completion of Part 5, to include the Python-generated .csv file

--- Challenges/loan_analyzer.py
+++ Challenges/loan_analyzer.py
@@ -203,3 +203,12 @@ output_path = Path("inexpensive_loans.csv")
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
 # YOUR CODE HERE!
+
+csvpath = Path("inexpensive_loans.csv")
+with open(csvpath, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(header)
+    for row in loans:
+        csvwriter.writerow(row.values())
+
+
